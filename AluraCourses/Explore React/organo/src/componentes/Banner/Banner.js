eb4cca6d521d689a0152a3d2aa3c3bfed2b6ec92
Gabriel Rossi <gabriel.projetosgit@gmail.com>
2023-05-14 01:03:45 -0300

    projeto finalizado

--- AluraCourses/Explore React/organo/src/componentes/Banner/Banner.js
+++ AluraCourses/Explore React/organo/src/componentes/Banner/Banner.js
@@ -4,6 +4,7 @@ import "./Banner.css"
 function Banner() {
     return (
         //serve para criar cadeia de elementos sem precisar estar dentro de uma div
+        //<></>
         <Fragment> 
             <header className="banner">
                 <img src="/imagens/banner.png" alt="O Banner principal da página do Organo"/>
