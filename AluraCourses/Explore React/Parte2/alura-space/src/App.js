2b30decee3d418197007c9456659d7d39a0d543f
Gabriel Rossi <gabriel.projetosgit@gmail.com>
2023-06-30 23:14:02 -0300

    cria component de menu

--- AluraCourses/Explore React/Parte2/alura-space/src/App.js
+++ AluraCourses/Explore React/Parte2/alura-space/src/App.js
@@ -1,6 +1,7 @@
-import Cabecalho from "./componentes/cabecalho";
+import PaginaInicial from "./Paginas/PaginaInicial";
+import Cabecalho from "./componentes/Cabecalho";
 import './styles/estilosGlobais.scss'
 
 export default function App() {
-  return <Cabecalho/>
+  return <PaginaInicial/>;
 }
--- AluraCourses/Explore React/Parte2/alura-space/src/Paginas/PaginaInicial/index.jsx
+++ AluraCourses/Explore React/Parte2/alura-space/src/Paginas/PaginaInicial/index.jsx
@@ -1,5 +1,23 @@
+import Menu from "../../componentes/Menu";
+import { Fragment } from "react";
+import Cabecalho from "../../componentes/Cabecalho";
+import styles from "./PaginaInicial.module.scss";
+import banner from "./banner.png";
+
+
 export default function PaginaInicial(){
     return (
-        <h1>Página Inicial</h1>
+        <>
+        <Cabecalho />
+        <main>
+            <section className={styles.principal}>
+            <Menu />
+            <div className={styles.principal__imagem}>
+                <h1>A galeria mais completa do espaço</h1>
+                <img src={banner} alt="A imagem da terra vista do espaço" />
+            </div>
+            </section>
+        </main>
+        </>
     )
 }
