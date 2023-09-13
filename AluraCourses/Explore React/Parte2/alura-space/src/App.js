57272baea1b0b62b9f58cd26648fbf3936b81b06
Gabriel Rossi <gabriel.projetosgit@gmail.com>
2023-09-12 23:38:27 -0300

    refactor

--- AluraCourses/Explore React/Parte2/alura-space/src/App.js
+++
@@ -1,7 +0,0 @@
-import PaginaInicial from "./Paginas/PaginaInicial";
-import Cabecalho from "./componentes/Cabecalho";
-import './styles/estilosGlobais.scss'
-
-export default function App() {
-  return <PaginaInicial/>;
-}
